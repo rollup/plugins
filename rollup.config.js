fc48b9ff189cbcb0c048909c9466c41533ea8625
Bogdan Chadkin <trysound@yandex.ru>
2016-02-01 13:26:03 +0300

    Fix build

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,6 @@ import babel from 'rollup-plugin-babel';
 
 export default {
     entry: 'src/index.js',
-    dest: 'dist/index.js',
-    format: 'cjs',
     external: [
         'path',
         'eslint',
