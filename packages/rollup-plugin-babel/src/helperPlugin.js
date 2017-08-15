31ed8615d7ff526d65e1ed0d63a18430061d9bfe
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2017-08-15 22:24:37 +0200

    Fix regression with Babel 6 (#158)
    * Fix regression with Babel 6
    
    Since in Babel 6 it is not supported to replace the argument
    of a default export declaration with an expression, use a
    custom "replaceWith" function which handles also that case.
    
    Fixes #155, fixes #156.
    
    * Run tests using both Babel 7 and Babel 6

--- packages/rollup-plugin-babel/src/helperPlugin.js
+++ packages/rollup-plugin-babel/src/helperPlugin.js
@@ -1,8 +1,25 @@
-export default function importHelperPlugin () {
+export default function importHelperPlugin ({ types: t }) {
+	/**
+	 * This function is needed because of a bug in Babel 6.x, which prevents the
+	 * declaration of an ExportDefaultDeclaration to be replaced with an
+	 * expression.
+	 * That bug has been fixed in Babel 7.
+	 */
+	function replaceWith (path, replacement) {
+		if (
+			path.parentPath.isExportDefaultDeclaration() &&
+			t.isExpression(replacement)
+		) {
+			path.parentPath.replaceWith(t.exportDefaultDeclaration(replacement));
+		} else {
+			path.replaceWith(replacement);
+		}
+	}
+
 	return {
 		visitor: {
 			ClassDeclaration (path, state) {
-				path.replaceWith(state.file.addHelper('classCallCheck'));
+				replaceWith(path, state.file.addHelper('classCallCheck'));
 			}
 		}
 	};
