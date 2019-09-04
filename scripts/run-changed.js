a393de3cd400c31aa9e5e328cdd6596bc5bb6e78
shellscape <andrew@shellscape.org>
2019-09-04 13:17:38 -0400

    chore: got the run-changed script working

--- scripts/run-changed.js
+++ scripts/run-changed.js
@@ -1,16 +1,18 @@
-const execa = require('execa');
+const execa = require('execa'); // eslint-disable-line import/no-extraneous-dependencies
 
-const { log } = console;
+const [, , task] = process.argv;
+const { stderr, stdout } = process;
 
 (async () => {
-  const rePkg = /(packages\/([\w\-\_\.]+))\/?/;
-  const { stdout } = await execa('git', ['diff', '--name-only']);
-  const filters = stdout.split('\n')
+  const rePkg = /(packages\/([\w\-_.]+))\/?/;
+  const { stdout: diff } = await execa('git', ['diff', 'master...HEAD', '--name-only']);
+  const filters = diff
+    .split('\n')
     .filter((line) => rePkg.test(line))
     .map((line) => {
-      const [,directory] = line.match(rePkg);
-      return '--filter ./' + directory;
+      const [, directory] = line.match(rePkg);
+      return `--filter ./${directory}`;
     });
-
-
+  const args = ['recursive', 'run', task].concat(Array.from(new Set(filters)));
+  await execa('pnpm', args, { stdout, stderr });
 })();
