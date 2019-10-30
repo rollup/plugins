55bbc3c769ac28ea6e805d13a661262fcf7d4bfc
shellscape <andrew@shellscape.org>
2019-10-30 08:46:24 -0400

    chore(release): strip v1.3.1

--- packages/strip/rollup.config.js
+++ packages/strip/rollup.config.js
@@ -1,9 +1,6 @@
 import pkg from './package.json';
 
 export default {
-	input: 'src/index.js',
-	output: [
-		{ format: 'cjs', file: pkg.main },
-		{ format: 'esm', file: pkg.module }
-	]
+  input: 'src/index.js',
+  output: [{ format: 'cjs', file: pkg.main }, { format: 'esm', file: pkg.module }]
 };
