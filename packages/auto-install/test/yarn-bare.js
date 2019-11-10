26da08d7cbb4f80b9783ef923ee5f817bbb4d569
Andrew Powell <shellscape@users.noreply.github.com>
2019-11-10 15:03:55 -0500

    chore: migrate plugin-auto-install (#33)
    * chore: add plugin-auto-install
    
    * chore: fix lint script
    
    * chore: upload lockfile
    
    * chore: fix lint script
    
    * chore: ci should install yarn
    
    * chore: trying to fix yarn snaps
    
    * chore: install yarn global in ci
    
    * chore: try removing extra whitespace
    
    * chore: bring yarn versions in line for same snaps
    
    * chore: remove ts test script call

---
+++ packages/auto-install/test/yarn-bare.js
@@ -0,0 +1,33 @@
+const { readFileSync } = require('fs');
+const { join } = require('path');
+
+const test = require('ava');
+const del = require('del');
+const resolve = require('rollup-plugin-node-resolve');
+const { rollup } = require('rollup');
+
+const autoInstall = require('..');
+
+const cwd = join(__dirname, 'fixtures/yarn-bare');
+const file = join(cwd, 'output/bundle.js');
+const input = join(cwd, '../input.js');
+
+process.chdir(cwd);
+
+test('yarn, bare', async (t) => {
+  await rollup({
+    input,
+    output: {
+      file,
+      format: 'cjs'
+    },
+    plugins: [autoInstall({ manager: 'yarn' }), resolve()]
+  });
+  const lock = readFileSync('yarn.lock', 'utf-8');
+  t.snapshot(readFileSync('package.json', 'utf-8'));
+  t.snapshot(lock);
+});
+
+test.after(async () => {
+  await del(['node_modules', 'package.json', 'yarn.lock']);
+});
