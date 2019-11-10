0bd164d14c5d2d5a7e4816c73213e52ca6b3e65d
shellscape <andrew@shellscape.org>
2019-11-10 13:40:25 -0500

    chore: add plugin-auto-install

---
+++ packages/auto-install/test/yarn-bare.js
@@ -0,0 +1,32 @@
+const { readFileSync } = require('fs');
+const { join } = require('path');
+
+const test = require('ava');
+const del = require('del');
+const resolve = require('rollup-plugin-node-resolve');
+const { rollup } = require('rollup');
+
+const autoInstall = require('..');
+
+const cwd = join(__dirname, 'fixtures/yarn-bare');
+const file = join(cwd, 'output/bundle.js');
+const input = join(cwd, '../input.js');
+
+process.chdir(cwd);
+
+test('yarn, bare', async (t) => {
+  await rollup({
+    input,
+    output: {
+      file,
+      format: 'cjs'
+    },
+    plugins: [autoInstall({ manager: 'yarn' }), resolve()]
+  });
+  t.snapshot(readFileSync('package.json', 'utf-8'));
+  t.snapshot(readFileSync('yarn.lock', 'utf-8'));
+});
+
+test.after(async () => {
+  await del(['node_modules', 'package.json', 'yarn.lock']);
+});
---
+++ packages/auto-install/test/yarn.js
@@ -0,0 +1,33 @@
+const { readFileSync, writeFileSync } = require('fs');
+const { join } = require('path');
+
+const test = require('ava');
+const del = require('del');
+const resolve = require('rollup-plugin-node-resolve');
+const { rollup } = require('rollup');
+
+const autoInstall = require('..');
+
+const cwd = join(__dirname, 'fixtures/yarn');
+const file = join(cwd, 'output/bundle.js');
+const input = join(cwd, '../input.js');
+const lockFile = join(cwd, 'yarn.lock');
+
+process.chdir(cwd);
+
+test('yarn', async (t) => {
+  await rollup({
+    input,
+    output: {
+      file,
+      format: 'cjs'
+    },
+    plugins: [autoInstall(), resolve()]
+  });
+  t.snapshot(readFileSync('yarn.lock', 'utf-8'));
+});
+
+test.after(async () => {
+  await del(['node_modules', 'package.json']);
+  writeFileSync(lockFile, '');
+});
