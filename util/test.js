2c2517a10b5612ca3dd82413cc1044eb79deb91d
Tiger Oakes <toakes@mozilla.com>
2019-11-27 22:56:06 -0800

    fix: Argument order in testBundle

--- util/test.js
+++ util/test.js
@@ -1,3 +1,7 @@
+/**
+ * Gets the code from a rollup bundle.
+ * @param {import('rollup').RollupBuild} bundle
+ */
 const getCode = async (bundle) => {
   const { output } = await bundle.generate({ format: 'cjs' });
   const [{ code }] = output;
@@ -5,6 +9,15 @@ const getCode = async (bundle) => {
   return code;
 };
 
+/**
+ * Runs a bundle for testing.
+ * A `result` variable is also inject that lets the script return some result.
+ * @param {import('ava').Assertions} t Ava assertions object.
+ * `t` is injected into the script so that test assertions can be used.
+ * @param {import('rollup').RollupBuild} bundle The rollup bundle to run.
+ * @param {object} args Additional arguments for the script.
+ * Each key is an argument name, and the value is the value passed to the function.
+ */
 const testBundle = async (t, bundle, args = {}) => {
   const { output } = await bundle.generate({ format: 'cjs' });
   const [{ code }] = output;
@@ -12,7 +25,7 @@ const testBundle = async (t, bundle, args = {}) => {
   // eslint-disable-next-line no-new-func
   const func = new Function(...params);
 
-  return { code, result: func(...[t, ...Object.values(args)]) };
+  return { code, result: func(...Object.values(args), t) };
 };
 
 module.exports = {
