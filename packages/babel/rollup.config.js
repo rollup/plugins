9c90bfca1614d1e5447fddf17b5647dd7feadece
Mateusz Burzyński <mateuszburzynski@gmail.com>
2019-12-23 20:11:29 +0100

    Drop transpilation of rollup-plugin-babel, set engines to node@>=10

--- packages/babel/rollup.config.js
+++ packages/babel/rollup.config.js
@@ -1,16 +1,13 @@
-import buble from 'rollup-plugin-buble';
 import pkg from './package.json';
 
 const externalDeps = Object.keys(Object.assign({}, pkg.dependencies, pkg.peerDependencies));
 const nodeDeps = ['path'];
-const external = externalDeps.concat(nodeDeps);
 
 export default {
 	input: './src/index.js',
-	plugins: [buble({ objectAssign: 'Object.assign', transforms: { asyncAwait: false } })],
-	external,
+	external: externalDeps.concat(nodeDeps),
 	output: [
-		{ file: pkg.main, format: 'cjs', sourcemap: true },
-		{ file: pkg.module, format: 'esm', sourcemap: true },
+		{ file: pkg.main, format: 'cjs' },
+		{ file: pkg.module, format: 'esm' },
 	],
 };
