b2c300b50a7141f4f25852f0b1a6317b691df0c1
Mateusz Burzyński <mateuszburzynski@gmail.com>
2019-12-25 22:11:53 +0100

    Transpile @rollup/plugin-babel using itself

--- packages/babel/rollup.config.js
+++ packages/babel/rollup.config.js
@@ -1,4 +1,5 @@
 import pkg from './package.json';
+import babel from './src/index';
 
 const externalDeps = Object.keys(Object.assign({}, pkg.dependencies, pkg.peerDependencies));
 const nodeDeps = ['path'];
@@ -6,6 +7,12 @@ const nodeDeps = ['path'];
 export default {
   input: './src/index.js',
   external: externalDeps.concat(nodeDeps),
+  plugins: [
+    babel({
+      presets: [['@babel/preset-env', { targets: { node: 8 } }]],
+      babelHelpers: 'bundled'
+    })
+  ],
   output: [
     { file: pkg.main, format: 'cjs', exports: 'named' },
     { file: pkg.module, format: 'esm' }
