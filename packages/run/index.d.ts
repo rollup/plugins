b454e0f03132c2536f89ff8d4945ae7ba8f7fca9
Carlos Precioso <cprecioso@users.noreply.github.com>
2019-12-26 04:30:40 +0100

    feat(run): add typings  (#115)
    * Add types for `run`
    
    * Use child_process options for plugin-run types

---
+++ packages/run/index.d.ts
@@ -0,0 +1,8 @@
+import { ForkOptions } from 'child_process'
+import { Plugin } from 'rollup'
+
+/**
+ * Run your bundles in Node once they're built
+ * @param options These are passed through to `child_process.fork(..)`
+ */
+export default function run(options?: ForkOptions): Plugin
