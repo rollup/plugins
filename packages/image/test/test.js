2eb670f6d53a54d0b5a67e5c2a6d80a913a67713
shellscape <andrew@shellscape.org>
2020-01-02 23:01:26 -0500

    chore: re-enable commented out tests

--- packages/image/test/test.js
+++ packages/image/test/test.js
@@ -7,31 +7,31 @@ const image = require('..');
 
 process.chdir(__dirname);
 
-// test('imports an image', async (t) => {
-//   const bundle = await rollup.rollup({
-//     input: 'fixtures/image.js',
-//     plugins: [image()]
-//   });
-//
-//   t.snapshot(await getCode(bundle));
-// });
-//
-// test('imports an image for the dom', async (t) => {
-//   const bundle = await rollup.rollup({
-//     input: 'fixtures/image.js',
-//     plugins: [image({ dom: true })]
-//   });
-//
-//   t.snapshot(await getCode(bundle));
-// });
-//
-// test('ignores invalid image', async (t) =>
-//   t.throwsAsync(async () =>
-//     rollup.rollup({
-//       input: 'fixtures/invalid-image.js',
-//       plugins: [image()]
-//     })
-//   ));
+test('imports an image', async (t) => {
+  const bundle = await rollup.rollup({
+    input: 'fixtures/image.js',
+    plugins: [image()]
+  });
+
+  t.snapshot(await getCode(bundle));
+});
+
+test('imports an image for the dom', async (t) => {
+  const bundle = await rollup.rollup({
+    input: 'fixtures/image.js',
+    plugins: [image({ dom: true })]
+  });
+
+  t.snapshot(await getCode(bundle));
+});
+
+test('ignores invalid image', async (t) =>
+  t.throwsAsync(async () =>
+    rollup.rollup({
+      input: 'fixtures/invalid-image.js',
+      plugins: [image()]
+    })
+  ));
 
 test('imports an svg without encoding', async (t) => {
   const bundle = await rollup.rollup({
