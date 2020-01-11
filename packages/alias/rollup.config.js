e36540f44a0b50a9e7238c40ad3a006e89af94a8
shellscape <andrew@shellscape.org>
2020-01-10 20:37:06 -0500

    chore(alias): update dependencies

--- packages/alias/rollup.config.js
+++ packages/alias/rollup.config.js
@@ -2,6 +2,9 @@ import pkg from './package.json';
 
 export default {
   input: 'src/index.js',
-  output: [{ file: pkg.main, format: 'cjs' }, { file: pkg.module, format: 'es' }],
-  external: Object.keys(pkg.dependencies).concat(['path', 'fs', 'os'])
+  external: [...Object.keys(pkg.dependencies), 'path', 'fs', 'os'],
+  output: [
+    { file: pkg.main, format: 'cjs' },
+    { file: pkg.module, format: 'es' }
+  ]
 };
