181731642fe11b151dc1b07bb43becf3a8aaf221
Mateusz Burzyński <mateuszburzynski@gmail.com>
2020-01-22 16:56:29 +0100

    Fixed how @rollup/pluginutils are used

--- packages/babel/src/index.js
+++ packages/babel/src/index.js
@@ -1,11 +1,13 @@
 import * as babel from '@babel/core';
-import { createFilter } from '@rollup/pluginutils';
+import pluginUtils from '@rollup/pluginutils';
 
 import { RUNTIME, EXTERNAL, BUNDLED, INLINE, HELPERS } from './constants';
 import bundledHelpersPlugin from './bundledHelpersPlugin';
 import preflightCheck from './preflightCheck';
 import transformCode from './transformCode';
 import { addBabelPlugin, escapeRegExpCharacters, warnOnce } from './utils';
+
+const { createFilter } = pluginUtils;
 
 const unpackOptions = ({
   extensions = babel.DEFAULT_EXTENSIONS,
