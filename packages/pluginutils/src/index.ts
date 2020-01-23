e0c1deccc248184aad095a47b412097d4ad6e7ce
Mateusz Burzyński <mateuszburzynski@gmail.com>
2020-01-23 09:44:04 +0100

    fix(pluginutils): bring back named exports

--- packages/pluginutils/src/index.ts
+++ packages/pluginutils/src/index.ts
@@ -5,6 +5,16 @@ import dataToEsm from './dataToEsm';
 import extractAssignedNames from './extractAssignedNames';
 import makeLegalIdentifier from './makeLegalIdentifier';
 
+export {
+  addExtension,
+  attachScopes,
+  createFilter,
+  dataToEsm,
+  extractAssignedNames,
+  makeLegalIdentifier
+};
+
+// TODO: remove this in next major
 export default {
   addExtension,
   attachScopes,
