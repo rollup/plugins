5495d2eac31eb5534fdeec73e61eb497f1984c92
Dennis Markgraf <info@dennismarkgraf.com>
2020-06-03 17:24:48 +0200

    fix(typescript): sync rollup and typescript file watch

--- packages/typescript/src/watchProgram.ts
+++ packages/typescript/src/watchProgram.ts
@@ -3,8 +3,16 @@ import { PluginContext } from 'rollup';
 import { buildDiagnosticReporter } from './diagnostics/emit';
 import { DiagnosticsHost } from './diagnostics/host';
 import { Resolver } from './moduleResolution';
+import { DiagnosticCategory } from 'typescript';
 
 type BuilderProgram = import('typescript').EmitAndSemanticDiagnosticsBuilderProgram;
+
+// @see https://github.com/microsoft/TypeScript/blob/master/src/compiler/diagnosticMessages.json
+enum DiagnosticCode {
+  FILE_CHANGE_DETECTED = 6032,
+  FOUND_1_ERROR_WATCHING_FOR_FILE_CHANGES = 6193,
+  FOUND_N_ERRORS_WATCHING_FOR_FILE_CHANGES = 6194
+}
 
 interface CreateProgramOptions {
   /** Formatting host used to get some system functions and emit type errors. */
@@ -13,8 +21,93 @@ interface CreateProgramOptions {
   parsedOptions: import('typescript').ParsedCommandLine;
   /** Callback to save compiled files in memory. */
   writeFile: import('typescript').WriteFileCallback;
+  /** Callback for the Typescript status reporter. */
+  status: import('typescript').WatchStatusReporter;
   /** Function to resolve a module location */
   resolveModule: Resolver;
+}
+
+type DeferredResolve = ((value?: boolean) => void) | (() => void)
+
+interface Deferred {
+  promise: Promise<boolean | void>;
+  resolve: DeferredResolve;
+}
+
+function createDeferred (timeout?: number): Deferred {
+  let promise: Promise<boolean | void>
+  let resolve: DeferredResolve = () => {}
+
+  if (timeout) {
+    promise = Promise.race<Promise<boolean>>([
+      new Promise(r => setTimeout(r, timeout, true)),
+      new Promise(r => resolve = r)
+    ]);
+  } else {
+    promise = new Promise(r => resolve = r);
+  }
+
+  return { promise, resolve };
+}
+
+/**
+ * Typescript watch program helper to sync Typescript watch status with Rollup hooks.
+ */
+export class WatchProgramHelper {
+  private _startDeferred: Deferred | null = null;
+  private _finishDeferred: Deferred | null = null;
+
+  watch (timeout: number = 1000) {
+    // Race watcher start promise against a timeout in case Typescript and Rollup change detection is not in sync.
+    this._startDeferred = createDeferred(timeout);
+    this._finishDeferred = createDeferred();
+  }
+
+  handleStatus (diagnostic: import('typescript').Diagnostic) {
+    // Fullfil deferred promises by Typescript diagnostic message codes.
+    if (diagnostic.category === DiagnosticCategory.Message) {
+      switch(diagnostic.code) {
+        case DiagnosticCode.FILE_CHANGE_DETECTED:
+          this.resolveStart();
+          break;
+
+        case DiagnosticCode.FOUND_1_ERROR_WATCHING_FOR_FILE_CHANGES:
+        case DiagnosticCode.FOUND_N_ERRORS_WATCHING_FOR_FILE_CHANGES:
+          this.resolveFinish();
+          break;
+
+        default:
+      }
+    }
+  }
+
+  resolveStart () {
+    if (this._startDeferred) {
+      this._startDeferred.resolve(false);
+      this._startDeferred = null;
+    }
+  }
+
+  resolveFinish () {
+    if (this._finishDeferred) {
+      this._finishDeferred.resolve();
+      this._finishDeferred = null;
+    }
+  }
+
+  async wait () {
+    if (this._startDeferred) {
+      const timeout = await this._startDeferred.promise;
+
+      // If there is no file change detected by Typescript skip deferred promises.
+      if (timeout) {
+        this._startDeferred = null;
+        this._finishDeferred = null;
+      }
+
+      await this._finishDeferred?.promise;
+    }
+  }
 }
 
 /**
@@ -26,7 +119,7 @@ interface CreateProgramOptions {
 function createWatchHost(
   ts: typeof import('typescript'),
   context: PluginContext,
-  { formatHost, parsedOptions, writeFile, resolveModule }: CreateProgramOptions
+  { formatHost, parsedOptions, writeFile, status, resolveModule }: CreateProgramOptions
 ): import('typescript').WatchCompilerHostOfFilesAndCompilerOptions<BuilderProgram> {
   const createProgram = ts.createEmitAndSemanticDiagnosticsBuilderProgram;
 
@@ -36,8 +129,7 @@ function createWatchHost(
     ts.sys,
     createProgram,
     buildDiagnosticReporter(ts, context, formatHost),
-    // Ignore watch status changes
-    () => {},
+    status,
     parsedOptions.projectReferences
   );
 
