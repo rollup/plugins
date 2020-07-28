0b25eabfb4a61bccc1ce8df29327c2f54d9e4fdd
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2020-07-28 07:40:39 +0200

    chore(dsv): update dependencies

--- packages/dsv/rollup.config.js
+++ packages/dsv/rollup.config.js
@@ -4,7 +4,7 @@ export default {
   input: 'src/index.js',
   external: [...Object.keys(pkg.dependencies), 'path'],
   output: [
-    { file: pkg.main, format: 'cjs' },
+    { file: pkg.main, format: 'cjs', exports: 'auto' },
     { file: pkg.module, format: 'es' }
   ]
 };
