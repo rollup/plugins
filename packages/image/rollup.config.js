0c57b089c5a319f4cbf49a67e4e9de239136dfe6
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2020-07-28 07:43:54 +0200

    chore(image): update dependencies

--- packages/image/rollup.config.js
+++ packages/image/rollup.config.js
@@ -6,7 +6,7 @@ export default {
   input: 'src/index.js',
   external: [...Object.keys(pkg.dependencies), 'fs', 'path'],
   output: [
-    { file: pkg.main, format: 'cjs', sourcemap: true },
+    { file: pkg.main, format: 'cjs', sourcemap: true, exports: 'auto' },
     { file: pkg.module, format: 'es', sourcemap: true }
   ],
   plugins: [buble()]
