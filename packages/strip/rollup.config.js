7ff3d0cf684182112985c219a1f6bb7d102b17a1
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2020-07-28 08:04:25 +0200

    chore(strip): update dependencies

--- packages/strip/rollup.config.js
+++ packages/strip/rollup.config.js
@@ -2,8 +2,9 @@ import pkg from './package.json';
 
 export default {
   input: 'src/index.js',
+  external: Object.keys(pkg.dependencies),
   output: [
-    { format: 'cjs', file: pkg.main },
+    { format: 'cjs', file: pkg.main, exports: 'auto' },
     { format: 'esm', file: pkg.module }
   ]
 };
