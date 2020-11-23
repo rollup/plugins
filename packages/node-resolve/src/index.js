12eca25f85da67f35bca4a08bec82f136ec7ad82
Tom Jenkinson <tom@tjenkinson.me>
2020-11-23 21:08:58 +0000

    add deprecation warnings/errors

--- packages/node-resolve/src/index.js
+++ packages/node-resolve/src/index.js
@@ -38,11 +38,52 @@ const defaults = {
 export const DEFAULTS = deepFreeze(deepMerge({}, defaults));
 
 export function nodeResolve(opts = {}) {
+  const warnings = [];
+
+  if (opts.customResolveOptions) {
+    const { customResolveOptions } = opts;
+    if (customResolveOptions.moduleDirectory) {
+      opts.moduleDirectories = Array.isArray(customResolveOptions.moduleDirectory)
+        ? customResolveOptions.moduleDirectory
+        : [customResolveOptions.moduleDirectory];
+
+      warnings.push(
+        'node-resolve: The `customResolveOptions.moduleDirectory` option has been deprecated. Use `moduleDirectories`, which must be an array.'
+      );
+    }
+
+    if (customResolveOptions.preserveSymlinks) {
+      throw new Error(
+        'node-resolve: `customResolveOptions.preserveSymlinks` is no longer an option. We now always use the rollup `preserveSymlinks` option.'
+      );
+    }
+
+    [
+      'basedir',
+      'package',
+      'extensions',
+      'includeCoreModules',
+      'readFile',
+      'isFile',
+      'isDirectory',
+      'realpath',
+      'packageFilter',
+      'pathFilter',
+      'paths',
+      'packageIterator'
+    ].forEach((resolveOption) => {
+      if (customResolveOptions[resolveOption]) {
+        throw new Error(
+          `node-resolve: \`customResolveOptions.${resolveOption}\` is no longer an option. If you need this, please open an issue.`
+        );
+      }
+    });
+  }
+
   const options = { ...defaults, ...opts };
   const { extensions, jail, moduleDirectories } = options;
   const conditionsEsm = [...baseConditionsEsm, ...(options.exportConditions || [])];
   const conditionsCjs = [...baseConditionsCjs, ...(options.exportConditions || [])];
-  const warnings = [];
   const packageInfoCache = new Map();
   const idToPackageInfo = new Map();
   const mainFields = getMainFields(options);
--- packages/node-resolve/test/test.js
+++ packages/node-resolve/test/test.js
@@ -178,6 +178,25 @@ test('allows custom moduleDirectories', async (t) => {
   t.is(bundle.cache.modules[0].id, resolve('custom-module-dir/js_modules/foo.js'));
 });
 
+test('allows custom moduleDirectories with legacy customResolveOptions.moduleDirectory', async (t) => {
+  const warnings = [];
+  const bundle = await rollup({
+    input: 'custom-module-dir/main.js',
+    onwarn: (warning) => warnings.push(warning),
+    plugins: [
+      nodeResolve({
+        customResolveOptions: {
+          moduleDirectory: 'js_modules'
+        }
+      })
+    ]
+  });
+
+  t.is(bundle.cache.modules[0].id, resolve('custom-module-dir/js_modules/foo.js'));
+  t.is(warnings.length, 1);
+  t.snapshot(warnings);
+});
+
 test('ignores deep-import non-modules', async (t) => {
   const warnings = [];
   const bundle = await rollup({
@@ -311,4 +330,34 @@ test('can resolve imports with search params and hash', async (t) => {
   const { module } = await testBundle(t, bundle);
 
   t.is(module.exports, 'resolved with search params and hash');
+});
+
+[
+  'preserveSymlinks',
+  'basedir',
+  'package',
+  'extensions',
+  'includeCoreModules',
+  'readFile',
+  'isFile',
+  'isDirectory',
+  'realpath',
+  'packageFilter',
+  'pathFilter',
+  'paths',
+  'packageIterator'
+].forEach((resolveOption) => {
+  test(`throws error for removed customResolveOptions.${resolveOption} option`, (t) => {
+    try {
+      nodeResolve({
+        customResolveOptions: {
+          [resolveOption]: 'something'
+        }
+      });
+    } catch (e) {
+      t.snapshot(e);
+      return;
+    }
+    t.fail('expecting error');
+  });
 });
