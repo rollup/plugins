1fd8c05fb93b31c1b1d27c0da5346f452574234a
shellscape <andrew@shellscape.org>
2021-02-03 09:23:36 -0500

    fix: add 2015 to list of valid ModuleKinds

--- packages/typescript/src/preflight.ts
+++ packages/typescript/src/preflight.ts
@@ -8,10 +8,12 @@ Rollup requires that TypeScript produces ES Modules. Unfortunately your configur
  "module" other than "esnext". Unless you know what you're doing, please change "module" to "esnext"
  in the target tsconfig.json file or plugin options.`.replace(/\n/g, '');
 
+let undef;
+const validModules = [ModuleKind.ES2015, ModuleKind.ES2020, ModuleKind.ESNext, undef];
+
 // eslint-disable-next-line import/prefer-default-export
 export const preflight = (config: TypeScriptConfig, context: PluginContext) => {
-  let undef;
-  if (![ModuleKind.ESNext, ModuleKind.ES2020, undef].includes(config.options.module)) {
+  if (!validModules.includes(config.options.module)) {
     context.warn(`@rollup/plugin-typescript: ${moduleError}`);
   }
 };
