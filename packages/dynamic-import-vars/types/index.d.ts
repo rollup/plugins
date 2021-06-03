a0e3327812a407e1f955927c1bd46080d85ff622
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-06-03 11:07:14 -0400

    feat(dynamic-import-vars): add typings

---
+++ packages/dynamic-import-vars/types/index.d.ts
@@ -0,0 +1,33 @@
+import { FilterPattern } from '@rollup/pluginutils';
+import { Node } from 'estree-walker';
+import { Plugin } from 'rollup';
+
+interface RollupDynamicImportVariablesOptions {
+  /**
+   * A minimatch pattern, or array of patterns, which specifies the files in the build the plugin
+   * should operate on.
+   * By default all files are targeted.
+   */
+  include?: FilterPattern;
+  /**
+   * A minimatch pattern, or array of patterns, which specifies the files in the build the plugin
+   * should _ignore_.
+   * By default no files are ignored.
+   */
+  exclude?: FilterPattern;
+  /**
+   * By default, the plugin quits the build process when it encounters an error.
+   * If you set this option to true, it will throw a warning instead and leave the code untouched.
+   * @default false
+   */
+  warnOnError?: boolean;
+}
+
+export class VariableDynamicImportError extends Error {}
+
+export function dynamicImportToGlob(node: Node, sourceString: string): null | string;
+
+/**
+ * Support variables in dynamic imports in Rollup.
+ */
+export default function dynamicImportVariables(): Plugin;
