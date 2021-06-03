992e90477914068d7a5caf09790e2c41ab7f8b5e
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-06-03 11:21:00 -0400

    feat(legacy): add typings

---
+++ packages/legacy/types/index.d.ts
@@ -0,0 +1,10 @@
+import { Plugin } from 'rollup';
+
+interface RollupLegacyOptions {
+  [key: string]: string | { [key: string]: string };
+}
+
+/**
+ * A Rollup plugin which adds `export` declarations to legacy non-module scripts.
+ */
+export default function legacy(options: RollupLegacyOptions): Plugin;
