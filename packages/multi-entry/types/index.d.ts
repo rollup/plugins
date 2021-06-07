d730c3d11c5ea91e03b63a431aaee638e1a76733
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-06-07 08:42:23 -0400

    chore(types): make options optional

--- packages/multi-entry/types/index.d.ts
+++ packages/multi-entry/types/index.d.ts
@@ -1,31 +1,31 @@
-import { FilterPattern } from '@rollup/pluginutils';
-import { Plugin } from 'rollup';
+import {FilterPattern} from '@rollup/pluginutils'
+import {Plugin} from 'rollup'
 
 interface RollupMultiEntryOptions {
-  /**
-   * A minimatch pattern, or array of patterns, which specifies the files in the build the plugin
-   * should operate on.
-   * By default all files are targeted.
-   */
-  include?: FilterPattern;
-  /**
-   * A minimatch pattern, or array of patterns, which specifies the files in the build the plugin
-   * should _ignore_.
-   * By default no files are ignored.
-   */
-  exclude?: FilterPattern;
-  /**
-   * - If `true`, instructs the plugin to export named exports to the bundle from all entries.
-   * - If `false`, the plugin will not export any entry exports to the bundle.
-   * @default true
-   */
-  exports?: boolean;
-  /**
-   * `entryFileName` changes the name of the generated entry file.
-   * By default, it will override `outputOptions.entryFileNames` to be `'multi-entry.js'`.
-   * @default 'multi-entry.js'
-   */
-  entryFileName?: string;
+    /**
+     * A minimatch pattern, or array of patterns, which specifies the files in the build the plugin
+     * should operate on.
+     * By default all files are targeted.
+     */
+    include?: FilterPattern
+    /**
+     * A minimatch pattern, or array of patterns, which specifies the files in the build the plugin
+     * should _ignore_.
+     * By default no files are ignored.
+     */
+    exclude?: FilterPattern
+    /**
+     * - If `true`, instructs the plugin to export named exports to the bundle from all entries.
+     * - If `false`, the plugin will not export any entry exports to the bundle.
+     * @default true
+     */
+    exports?: boolean
+    /**
+     * `entryFileName` changes the name of the generated entry file.
+     * By default, it will override `outputOptions.entryFileNames` to be `'multi-entry.js'`.
+     * @default 'multi-entry.js'
+     */
+    entryFileName?: string
 }
 
 /**
@@ -34,4 +34,4 @@ interface RollupMultiEntryOptions {
  * _Note: `default` exports cannot be combined and exported by this plugin. Only named exports
  * will be exported._
  */
-export default function multiEntry(options: RollupMultiEntryOptions): Plugin;
+export default function multiEntry(options?: RollupMultiEntryOptions): Plugin
