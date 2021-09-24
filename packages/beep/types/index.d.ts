5f53f5533630c2ba14bcd91df35da3fd6eae0db5
Alison Stump <astump@tesla.com>
2021-09-24 14:41:43 -0700

    fix: update types for published beep

--- packages/beep/types/index.d.ts
+++ packages/beep/types/index.d.ts
@@ -1,6 +1,14 @@
 import { Plugin } from 'rollup';
 
+export interface RollupPluginBeepOptions {
+  error?: boolean;
+  warning?: boolean;
+}
+
 /**
  * 🍣 A Rollup plugin that beeps when a build ends with errors.
+ * @param options - Plugin options.
+ * @returns Plugin instance.
  */
-export default function beep(): Plugin;
+export function beep(options?: RollupPluginBeepOptions): Plugin;
+export default beep;
