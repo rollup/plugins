335a013b4d7f4b6a13caed8dcc6d5d953f9fc5a0
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2021-11-22 07:08:01 +0100

    refactor(commonjs): deconflict helpers only once globals are known (#1038)

--- packages/commonjs/test/fixtures/function/dynamic-module-require/_config.js
+++ packages/commonjs/test/fixtures/function/dynamic-module-require/_config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  // TODO Lukas think about a way to re-implement
-  skip: true,
   description: 'supports dynamic require',
   pluginOptions: {
     dynamicRequireTargets: ['fixtures/function/dynamic-module-require/submodule.js']
