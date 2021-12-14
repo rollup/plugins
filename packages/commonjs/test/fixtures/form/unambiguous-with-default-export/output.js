29074f4e98abf10ec47085b0b3381246118e8a5d
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2021-12-14 06:30:47 +0100

    fix(commonjs): inject module name into dynamic require function (#1038)

--- packages/commonjs/test/fixtures/form/unambiguous-with-default-export/output.js
+++ packages/commonjs/test/fixtures/form/unambiguous-with-default-export/output.js
@@ -1,5 +1,4 @@
 import * as commonjsHelpers from "_commonjsHelpers.js";
-import { commonjsRequire as commonjsRequire } from "_commonjs-dynamic-modules";
 import { __exports as input } from "\u0000fixtures/form/unambiguous-with-default-export/input.js?commonjs-exports"
 import "\u0000CWD/fixtures/form/unambiguous-with-default-export/foo.js?commonjs-proxy";
 
--- packages/commonjs/test/fixtures/form/unambiguous-with-import/output.js
+++ packages/commonjs/test/fixtures/form/unambiguous-with-import/output.js
@@ -1,5 +1,4 @@
 import * as commonjsHelpers from "_commonjsHelpers.js";
-import { commonjsRequire as commonjsRequire } from "_commonjs-dynamic-modules";
 import { __exports as input } from "\u0000fixtures/form/unambiguous-with-import/input.js?commonjs-exports"
 import "\u0000CWD/fixtures/form/unambiguous-with-import/foo.js?commonjs-proxy";
 
