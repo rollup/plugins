466c76e5de44145e5986a39940c97df6684822b6
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2022-04-03 07:26:36 +0200

    fix(commonjs): Do not change semantics when removing requires in if statements
    fix(commonjs): Allow to dynamically require an empty file

---
+++ packages/commonjs/test/fixtures/function/dynamic-require-empty/_config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  description: 'allows dynamically requiring an empty file',
+  pluginOptions: {
+    dynamicRequireTargets: ['fixtures/function/dynamic-require-empty/submodule.js']
+  }
+};
