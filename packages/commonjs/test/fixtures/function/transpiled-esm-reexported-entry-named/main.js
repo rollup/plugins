981918d70cd6607c2fc83dcfec607572db12c65e
François Wouts <f@zenc.io>
2022-04-19 19:59:41 +1000

    Remove comment given other tests do exactly the same
    See:
    - https://github.com/rollup/plugins/blob/d637611a79a2701c359f5f2f6ffb49978070da38/packages/commonjs/test/fixtures/function/transpiled-esm-entry-named/main.js#L3
    - https://github.com/rollup/plugins/blob/d637611a79a2701c359f5f2f6ffb49978070da38/packages/commonjs/test/fixtures/function/transpiled-esm-namespace-named/main.js#L5

--- packages/commonjs/test/fixtures/function/transpiled-esm-reexported-entry-named/main.js
+++ packages/commonjs/test/fixtures/function/transpiled-esm-reexported-entry-named/main.js
@@ -1,8 +1,5 @@
 import * as entry from './proxy';
 
-// Note: There ideally shouldn't be a default generated property.
-//
-// This should however be harmless.
 t.deepEqual(entry, {
   default: {
     named: 'named',
