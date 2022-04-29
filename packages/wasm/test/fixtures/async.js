5fa2bc8d9537e7e31ae7035c6cdc650e195f0b8e
Nick Babcock <nbabcock19@hotmail.com>
2022-04-29 18:41:28 -0500

    test(wasm): reference atob polyfill in browser test (#1179)
    The "target environment browser" test invoked the bundled output, which
    leads to an undefined error on node 14 as [`atob` was introduced in node
    16][0].
    
    This commit uses the same atob polyfill that was introduced in #334
    
    [0]: https://developer.mozilla.org/en-US/docs/Web/API/atob#browser_compatibility

--- packages/wasm/test/fixtures/async.js
+++ packages/wasm/test/fixtures/async.js
@@ -1,6 +1,9 @@
 import fixture from './sample.wasm';
 
 // eslint-disable-next-line no-undef
+globalThis.atob = (str) => Buffer.from(str, 'base64').toString('binary');
+
+// eslint-disable-next-line no-undef
 result = (async () => {
   const module = await fixture();
   let { instance } = await fixture({ env: {} });
