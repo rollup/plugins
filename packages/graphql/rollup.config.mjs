2dcffa74d6e36aa01ba3350cee57334988cc7f41
Lukas Taegert-Atkinson <lukastaegert@users.noreply.github.com>
2022-10-09 06:38:54 +0200

    fix(graphql): prepare for Rollup 3 (#1295)
    BREAKING CHANGES: Requires Node 14

---
+++ packages/graphql/rollup.config.mjs
@@ -0,0 +1,14 @@
+import { readFileSync } from 'fs';
+
+import buble from '@rollup/plugin-buble';
+
+import { createConfig } from '../../shared/rollup.config.mjs';
+
+export default {
+  ...createConfig({
+    pkg: JSON.parse(readFileSync(new URL('./package.json', import.meta.url), 'utf8')),
+    external: ['graphql-tag/loader']
+  }),
+  input: 'src/index.js',
+  plugins: [buble()]
+};
