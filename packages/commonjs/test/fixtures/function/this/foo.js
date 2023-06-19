b844adb3e24dfd3b5576406d4c535756f48ee696
XiaoPi <530257315@qq.com>
2023-06-19 21:42:46 +0800

    fix(commonjs): add classBodyDepth flag (#1507)

--- packages/commonjs/test/fixtures/function/this/foo.js
+++ packages/commonjs/test/fixtures/function/this/foo.js
@@ -1,5 +1,17 @@
-module.exports = function augmentThis() {
+exports.augmentThis = function augmentThis() {
   this.x = 'x';
 };
 
 this.y = 'y';
+
+exports.classThis = class classThis {
+  constructor(){
+    class _classThis {
+      y = 'yyy'
+      yyy = this.y
+    }
+    this._instance = new _classThis()
+  }
+  y = 'yy'
+  yy = this.y
+}
--- packages/commonjs/test/fixtures/function/this/main.js
+++ packages/commonjs/test/fixtures/function/this/main.js
@@ -1,7 +1,12 @@
-const foo = require('./foo');
+const { augmentThis, classThis } = require('./foo');
 
 const obj = {};
-foo.call(obj);
+augmentThis.call(obj);
 
 t.is(obj.x, 'x');
 t.is(this.y, 'y');
+
+const instance = new classThis();
+
+t.is(instance.yy,'yy');
+t.is(instance._instance.yyy,'yyy');
