5235c75455e81e19a55004c98cc7d0e01723b30a
tada5hi <peter.placzek1996@gmail.com>
2023-11-05 20:18:27 +0100

    feat(swc): allow passing env property as option

--- packages/swc/test/test.js
+++ packages/swc/test/test.js
@@ -83,3 +83,47 @@ test.serial('work with source map', async (t) => {
   t.is(sourceMap.fileName, 'unminified.js.map');
   t.is(sourceMap.type, 'asset');
 });
+
+test.serial('allow passing swc.env', async (t) => {
+  const bundle = await rollup({
+    input: 'test/fixtures/export-default.js',
+    plugins: [
+      swc({
+        env: {
+          targets: ['es2020'],
+          shippedProposals: true
+        }
+      })
+    ]
+  });
+  const result = await bundle.generate({ format: 'cjs' });
+  t.is(result.output.length, 1);
+  const [output] = result.output;
+  t.is(output.code, "'use strict';\n\nvar exportDefault = 5;\n\nmodule.exports = exportDefault;\n");
+});
+
+test.serial('catch error on jsc and env option', async (t) => {
+  try {
+    const bundle = await rollup({
+      input: 'test/fixtures/export-default.js',
+      plugins: [
+        swc({
+          swc: {
+            jsc: {
+              target: 'es2020'
+            },
+            env: {
+              targets: ['es2022'],
+              shippedProposals: true
+            }
+          }
+        })
+      ]
+    });
+
+    await bundle.generate({ format: 'cjs' });
+    t.is(0, 1);
+  } catch (e) {
+    t.is(e.message, '`env` and `jsc.target` cannot be used together');
+  }
+});
