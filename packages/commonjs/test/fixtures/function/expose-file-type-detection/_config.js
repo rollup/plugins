0e08617fcd659077238f79e96e4336a013687f3e
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2024-01-18 07:15:12 +0100

    test: adapt assertions for wrapped CommonJS

--- packages/commonjs/test/fixtures/function/expose-file-type-detection/_config.js
+++ packages/commonjs/test/fixtures/function/expose-file-type-detection/_config.js
@@ -4,30 +4,29 @@ const ID_MAIN = path.join(__dirname, 'main.js');
 const ID_OTHER = path.join(__dirname, 'other.js');
 
 module.exports = {
-  description: 'exposes cjs file type detection to other plugins (strictRequires: "auto")',
+  description: 'exposes cjs file type detection to other plugins',
   options: {
     input: [ID_MAIN, ID_OTHER],
     plugins: [
       {
         moduleParsed({ id, meta: { commonjs } }) {
           if (id === ID_OTHER) {
-            if (commonjs.isCommonJS !== true) {
+            if (commonjs.isCommonJS !== 'withRequireFunction') {
               throw new Error(
                 `File "${id}" wrongly detected: isCommonJS === ${JSON.stringify(
                   commonjs.isCommonJS
-                )}`
+                )} instead of "withRequireFunction"`
               );
             }
-          } else if (commonjs && commonjs.isCommonJS !== false) {
+          } else if (commonjs && !!commonjs.isCommonJS) {
             throw new Error(
-              `File "${id}" wrongly detected: isCommonJS === ${JSON.stringify(commonjs.isCommonJS)}`
+              `File "${id}" wrongly detected: isCommonJS === ${JSON.stringify(
+                commonjs.isCommonJS
+              )} instead of false`
             );
           }
         }
       }
     ]
-  },
-  pluginOptions: {
-    strictRequires: 'auto'
   }
 };
