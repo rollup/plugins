3121646454c5dd29eaba3fec5a872f6249d3ed7e
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2024-01-18 06:32:31 +0100

    test: correctly pass options in test
    Unfortunately, I did not find a way to detect this case from the plugin
    and show a warning.

--- packages/commonjs/test/fixtures/function/pass-require-to-node-resolve/_config.js
+++ packages/commonjs/test/fixtures/function/pass-require-to-node-resolve/_config.js
@@ -1,26 +1,23 @@
 const assert = require('assert');
 
 module.exports = {
-  description: 'notifies the node-resolve plugin if an id is imported via "require" (strictRequires: "auto")',
+  description: 'notifies the node-resolve plugin if an id is imported via "require"',
   options: {
     plugins: [
       {
         name: 'node-resolve-mock',
-        resolveId(source, importer, { custom }) {
-          const { isRequire } = (custom && custom['node-resolve']) || {};
+        resolveId(source, importer, options) {
+          const { isRequire } = options.custom?.['node-resolve'] || {};
           if (source === './foo') {
-            return this.resolve(isRequire ? './foo-required' : './foo-imported', importer);
+            return this.resolve(isRequire ? './foo-required' : './foo-imported', importer, options);
           }
           if (source === './bar') {
-            return this.resolve(isRequire ? './bar-required' : './bar-imported', importer);
+            return this.resolve(isRequire ? './bar-required' : './bar-imported', importer, options);
           }
           return null;
         }
       }
     ]
-  },
-  pluginOptions: {
-    strictRequires: 'auto'
   },
   async exports(exports) {
     assert.deepStrictEqual(await exports, [{ default: 'imported' }, { default: 'imported' }]);
