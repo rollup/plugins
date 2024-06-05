cd00ee0539890187c863b35ec079127d7d4cec60
Jonathan Coates <git@squiddev.cc>
2024-06-05 20:07:14 +0100

    fix(html): Correct types for the template function
     - Make templateoptions non-optional
     - Allow template to return a Promise
    
    Also adds type annotations to the implementation to ensure the types are
    more consistent.

--- packages/html/types/index.d.ts
+++ packages/html/types/index.d.ts
@@ -15,7 +15,7 @@ export interface RollupHtmlOptions {
   fileName?: string;
   meta?: Record<string, any>[];
   publicPath?: string;
-  template?: (templateoptions?: RollupHtmlTemplateOptions) => string;
+  template?: (templateoptions: RollupHtmlTemplateOptions) => string | Promise<string>;
 }
 
 export function makeHtmlAttributes(attributes: Record<string, string>): string;
