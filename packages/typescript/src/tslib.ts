d3610859eaef11395ccc0a1d70cd239a4dcbdc57
Abhijeet Prasad <aprasad@sentry.io>
2024-06-28 20:45:58 -0400

    ref(typescript): Remove resolve package
    @rollup/plugin-typescript requires Node 14 or higher, so we can switch
    from using the resolve package to built-in `require.resolve`.

--- packages/typescript/src/tslib.ts
+++ packages/typescript/src/tslib.ts
@@ -1,29 +1,21 @@
 import { fileURLToPath } from 'url';
 
-import type { SyncOpts } from 'resolve';
-import resolve from 'resolve';
-
-// const resolveIdAsync = (file: string, opts: AsyncOpts) =>
-//   new Promise<string>((fulfil, reject) =>
-//     resolveId(file, opts, (err, contents) =>
-//       err || typeof contents === 'undefined' ? reject(err) : fulfil(contents)
-//     )
-//   );
-
-const resolveId = (file: string, opts: SyncOpts) => resolve.sync(file, opts);
+import { createRequire } from 'module';
 
 /**
  * Returns code asynchronously for the tslib helper library.
  */
 export const getTsLibPath = () => {
+  // @ts-ignore import.meta.url is allowed because the Rollup plugin injects the correct module format
+  const require = createRequire(import.meta.url);
   // Note: This isn't preferable, but we've no other way to test this bit. Removing the tslib devDep
   //       during the test run doesn't work due to the nature of the pnpm flat node_modules, and
   //       other workspace dependencies that depenend upon tslib.
   try {
     // eslint-disable-next-line no-underscore-dangle
-    return resolveId(process.env.__TSLIB_TEST_PATH__ || 'tslib/tslib.es6.js', {
-      // @ts-ignore import.meta.url is allowed because the Rollup plugin injects the correct module format
-      basedir: fileURLToPath(new URL('.', import.meta.url))
+    return require.resolve(process.env.__TSLIB_TEST_PATH__ || 'tslib/tslib.es6.js', {
+      // @ts-ignore See import.meta.url above
+      paths: [fileURLToPath(new URL('.', import.meta.url))]
     });
   } catch (_) {
     return null;
