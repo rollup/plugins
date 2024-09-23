85384e8d00384252365c9e959b639eb8060bef98
_Kerman <kermanx@qq.com>
2024-09-23 09:47:53 +0800

    fix: use `win32.sep` and `posix.sep`

--- packages/pluginutils/src/normalizePath.ts
+++ packages/pluginutils/src/normalizePath.ts
@@ -1,7 +1,11 @@
+import { win32, posix } from 'path';
+
 import type { NormalizePath } from '../types';
 
+const normalizePathRegExp = new RegExp(`\\${win32.sep}`, 'g');
+
 const normalizePath: NormalizePath = function normalizePath(filename: string) {
-  return filename.replace(/\\/g, '/');
+  return filename.replace(normalizePathRegExp, posix.sep);
 };
 
 export { normalizePath as default };
