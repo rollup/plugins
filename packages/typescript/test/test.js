rollup/plugins

commit a85b6491baf40ca13da3b98cd7df5dff35f6c774

fix(typescript)!: correctly resolve filenames of declaration files for `output.file` (#1728)
* test(typescript): add test case for invalid declarationDir with output.file

* fix(typescript): correctly resolve output filename of declaration files when output.file is used

* fix(typescript): validate that declarationDir is inside bundle output directory when using output.file

* test(typescript): check for correct error for invalid declarationDir when using output.file

---------

Co-authored-by: eu ler <27113373+u018f@users.noreply.github.com>

packages/typescript/src/options/validate.ts
@@ -1,4 +1,4 @@
-import { relative } from 'path';
+import { relative, dirname } from 'path';
 
 import type { OutputOptions, PluginContext } from 'rollup';
 
@@ -51,14 +51,24 @@ export function validatePaths(
     );
   }
 
+  let outputDir: string | undefined = outputOptions.dir;
+  if (outputOptions.file) {
+    outputDir = dirname(outputOptions.file);
+  }
   for (const dirProperty of DIRECTORY_PROPS) {
-    if (compilerOptions[dirProperty] && outputOptions.dir) {
+    if (compilerOptions[dirProperty] && outputDir) {
       // Checks if the given path lies within Rollup output dir
-      const fromRollupDirToTs = relative(outputOptions.dir, compilerOptions[dirProperty]!);
+      const fromRollupDirToTs = relative(outputDir, compilerOptions[dirProperty]!);
       if (fromRollupDirToTs.startsWith('..')) {
-        context.error(
-          `@rollup/plugin-typescript: Path of Typescript compiler option '${dirProperty}' must be located inside Rollup 'dir' option.`
-        );
+        if (outputOptions.dir) {
+          context.error(
+            `@rollup/plugin-typescript: Path of Typescript compiler option '${dirProperty}' must be located inside Rollup 'dir' option.`
+          );
+        } else {
+          context.error(
+            `@rollup/plugin-typescript: Path of Typescript compiler option '${dirProperty}' must be located inside the same directory as the Rollup 'file' option.`
+          );
+        }
       }
     }
   }

packages/typescript/test/test.js
@@ -101,6 +101,34 @@ test.serial('ensures declarationDir is located in Rollup output dir', async (t) 
   );
 });
 
+test.serial(
+  'ensures declarationDir is located in Rollup output directory when output.file is used',
+  async (t) => {
+    const bundle = await rollup({
+      input: 'fixtures/basic/main.ts',
+      plugins: [
+        typescript({
+          tsconfig: 'fixtures/basic/tsconfig.json',
+          declarationDir: 'fixtures/basic/other/',
+          declaration: true
+        })
+      ],
+      onwarn
+    });
+
+    // this should throw an error just like the equivalent setup using output.dir above
+    const wrongDirError = await t.throwsAsync(() =>
+      getCode(bundle, { format: 'es', file: 'fixtures/basic/dist/index.js' }, true)
+    );
+    t.true(
+      wrongDirError.message.includes(
+        `Path of Typescript compiler option 'declarationDir' must be located inside the same directory as the Rollup 'file' option`
+      ),
+      `Unexpected error message: ${wrongDirError.message}`
+    );
+  }
+);
+
 test.serial('ensures multiple outputs can be built', async (t) => {
   // In a rollup.config.js we would pass an array
   // The rollup method that's exported as a library won't do that so we must make two calls

packages/typescript/src/index.ts
@@ -178,20 +178,8 @@ export default function typescript(options: RollupTypescriptOptions = {}): Plugi
         if (outputOptions.dir) {
           baseDir = outputOptions.dir;
         } else if (outputOptions.file) {
-          // find common path of output.file and configured declation output
-          const outputDir = path.dirname(outputOptions.file);
-          const configured = path.resolve(
-            parsedOptions.options.declarationDir ||
-              parsedOptions.options.outDir ||
-              tsconfig ||
-              process.cwd()
-          );
-          const backwards = path
-            .relative(outputDir, configured)
-            .split(path.sep)
-            .filter((v) => v === '..')
-            .join(path.sep);
-          baseDir = path.normalize(`${outputDir}/${backwards}`);
+          // the bundle output directory used by rollup when outputOptions.file is used instead of outputOptions.dir
+          baseDir = path.dirname(outputOptions.file);
         }
         if (!baseDir) return;
 

commit f58c790b4f64010f200a269e1e19715e6013d07c

fix(typescript): allow for files to be nested within outDir
This change corrects a regression introduced by #1728, preventing dual bundle configs from writing output files to directories nested within the directory defined in the tsconfig's outDir property.

Example:
```js
export default {
  input: 'src/index.ts', // Replace with the path to your entry TypeScript file
  output: [
    {
      dir: 'dist/cjs',
      format: 'cjs',
      sourcemap: true,
      preserveModules: true
    },
    {
      dir: 'dist/esm',
      format: 'esm',
      sourcemap: true,
      preserveModules: true
    }
  ],
  plugins: [
    json(),
    resolve(),
    commonjs(),
    typescript({
      tsconfig: 'tsconfig.json',
      compilerOptions: {
        /* Basic Options */
        target: 'ES5',
        module: 'ES2020',
        lib: ['ES2019', 'ES2020'],
        allowJs: true,
        checkJs: false,
        /* Strict Type-Checking Options */
        strict: true,
        /* Module Resolution Options */
        moduleResolution: 'node',
        esModuleInterop: true,
        /* Advanced Options */
        forceConsistentCasingInFileNames: true,
        skipDefaultLibCheck: true,
        skipLibCheck: true,
        outDir: path.join(__dirname, 'dist')
      }
    })
  ],
  external: deps // Add any external dependencies you don't want to bundle
};
```

packages/typescript/src/options/validate.ts
@@ -58,13 +58,16 @@ export function validatePaths(
   for (const dirProperty of DIRECTORY_PROPS) {
     if (compilerOptions[dirProperty] && outputDir) {
       // Checks if the given path lies within Rollup output dir
-      const fromRollupDirToTs = relative(outputDir, compilerOptions[dirProperty]!);
-      if (fromRollupDirToTs.startsWith('..')) {
-        if (outputOptions.dir) {
+      if (outputOptions.dir) {
+        const fromRollupDirToTs = relative(outputDir, compilerOptions[dirProperty]!);
+        if (fromRollupDirToTs.startsWith('..')) {
           context.error(
             `@rollup/plugin-typescript: Path of Typescript compiler option '${dirProperty}' must be located inside Rollup 'dir' option.`
           );
-        } else {
+        }
+      } else {
+        const fromTsDirToRollup = relative(compilerOptions[dirProperty]!, outputDir);
+        if (fromTsDirToRollup.startsWith('..')) {
           context.error(
             `@rollup/plugin-typescript: Path of Typescript compiler option '${dirProperty}' must be located inside the same directory as the Rollup 'file' option.`
           );

packages/typescript/test/test.js
@@ -129,6 +129,36 @@ test.serial(
   }
 );
 
+test.serial(
+  'ensures output files can be written to subdirectories within the tsconfig outDir',
+  async (t) => {
+    const warnings = [];
+    const outputOpts = { format: 'es', file: 'fixtures/basic/dist/esm/main.js' };
+    const bundle = await rollup({
+      input: 'fixtures/basic/main.ts',
+      output: outputOpts,
+      plugins: [
+        typescript({
+          tsconfig: 'fixtures/basic/tsconfig.json',
+          outDir: 'fixtures/basic/dist'
+        })
+      ],
+      onwarn(warning) {
+        warnings.push(warning);
+      }
+    });
+
+    // This should not throw an error
+    const output = await getFiles(bundle, outputOpts);
+
+    t.deepEqual(
+      output.map((out) => out.fileName),
+      ['fixtures/basic/dist/esm/main.js']
+    );
+    t.is(warnings.length, 0);
+  }
+);
+
 test.serial('ensures multiple outputs can be built', async (t) => {
   // In a rollup.config.js we would pass an array
   // The rollup method that's exported as a library won't do that so we must make two calls