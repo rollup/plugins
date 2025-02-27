f23312a72cb7e6c0219da9f09051421f3e2d86a0
younggglcy <younggglcy@gmail.com>
2025-02-27 23:51:57 +0800

    fix(esm-shim): use `mlly` to find index to append shim (#1844)

--- packages/esm-shim/src/index.ts
+++ packages/esm-shim/src/index.ts
@@ -1,10 +1,11 @@
 /*
  * Copyright (c) 2023.
  * The core of this plugin was conceived by pi0 and is taken from the following repository:
- * https://github.com/unjs/unbuild/blob/main/src/builder/plugins/cjs.ts
+ * https://github.com/unjs/unbuild/blob/main/src/builders/rollup/plugins/cjs.ts
  */
 import { esmShim } from './module';
 
 export * from './types';
 export * from './utils';
 export default esmShim;
+export { esmShim };
