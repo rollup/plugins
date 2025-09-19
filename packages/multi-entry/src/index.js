868f007bca89477321dae69ed3ffcb7e288eccd5
Eike Reifhardt <eike.reifhardt@init.de>
2025-09-19 12:14:24 +0200

    fix(matched): replace matched with glob

--- packages/multi-entry/src/index.js
+++ packages/multi-entry/src/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-param-reassign */
 
 import virtual from '@rollup/plugin-virtual';
-import { promise as matched } from 'matched';
+import { glob } from 'glob';
 
 const DEFAULT_OUTPUT = 'multi-entry.js';
 const AS_IMPORT = 'import';
@@ -57,13 +57,27 @@ export default function multiEntry(conf = {}) {
       };
     },
 
-    buildStart(options) {
+    async buildStart(options) {
       const patterns = config.include.concat(config.exclude.map((pattern) => `!${pattern}`));
-      const entries = patterns.length
-        ? matched(patterns, { realpath: true }).then((paths) =>
-            paths.sort().map(exporter).join('\n')
-          )
-        : Promise.resolve('');
+
+      // Split patterns based on includes and excludes
+      const includePatterns = patterns.filter((pattern) => !pattern.startsWith('!'));
+      const excludePatterns = patterns
+        .filter((pattern) => pattern.startsWith('!'))
+        .map((pattern) => pattern.substring(1));
+
+      const paths = await Promise.all(
+        includePatterns.map(async (pattern) =>
+          glob(pattern, {
+            absolute: true,
+            ignore: excludePatterns
+          })
+        )
+      );
+
+      const flatPaths = [...new Set(paths.flat())];
+
+      const entries = flatPaths.length ? flatPaths.map(exporter).join('\n') : '';
 
       virtualisedEntry = virtual({ [options.input]: entries });
     },
